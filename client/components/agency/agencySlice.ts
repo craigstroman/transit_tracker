b15d44a792e297a3c105669ac341e6dddf57f436
Craig Stroman <cstroman@gmail.com>
2023-06-11 18:36:30 -0400

    Creating agency slice

---
+++ client/components/agency/agencySlice.ts
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../store/store';
+import { AgencyState, initialState } from './agencyTypes';
+import { getAgencies } from './agency.API';
+
+export const getAgenciesAsync = createAsyncThunk('agencies/get', async () => {
+  const response = await getAgencies();
+
+  return response.data;
+});
+
+export const agencySlice = createSlice({
+  name: 'agencies',
+  initialState,
+  reducers: {
+    resetState: () => {
+      const newState = initialState;
+
+      return newState;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAgenciesAsync.pending, (state) => {
+        const newState = state;
+        newState.status = 'loading';
+        return newState;
+      })
+      .addCase(getAgenciesAsync.fulfilled, (state, action) => {
+        const newState = state;
+        newState.status = 'success';
+        newState.value = action.payload;
+        return newState;
+      })
+      .addCase(getAgenciesAsync.rejected, (state) => {
+        const newState = state;
+        newState.status = 'not-found';
+        return newState;
+      });
+  },
+});
+
+export const { resetState } = agencySlice.actions;
+
+export const selectAgencyState = (state: RootState): AgencyState => state.agencies;
+
+export default agencySlice.reducer;
