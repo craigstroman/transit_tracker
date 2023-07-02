4054e2a2ab3df0d201af2e6699b6187e82964d87
Craig Stroman <cstroman@gmail.com>
2023-07-02 12:49:41 -0400

    Updating routes

--- client/containers/App.tsx
+++ client/containers/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
+import { Main } from './main/Main';
 import { Agency } from '../components/agency/Agency';
 import { Mode } from '../components/mode/Mode';
 import { Predictions } from './predictions/Predictions';
@@ -10,8 +11,7 @@ import { BusPredictions } from '../components/predictions/Bus/Bus';
 import { SubwayPredictions } from '../components/predictions/Subway/Subway';
 import { BusMap } from '../components/map/busMap/BusMap';
 import { SubwayMap } from '../components/map/subwayMap/SubwayMap';
-
-import Main from './main/Main';
+import './App.scss';
 
 const element = document.getElementById('app');
 const root = createRoot(element as HTMLDivElement);
@@ -24,22 +24,25 @@ root.render(
           <Route path="/" element={<Main />}>
             <Route path="/" element={<Agency />} />
             <Route path="/agency/:agency" element={<Mode />} />
-          </Route>
-          <Route element={<Predictions />}>
-            <Route
-              path="/agency/:agency/mode/3/routes/:route/direction/:direction/stops/:stop/predictions"
-              element={<BusPredictions />}
-            />
-            <Route
-              path="/agency/:agency/mode/1/routes/:route/stations/:station/direction/:direction/predictions"
-              element={<SubwayPredictions />}
-            />
-          </Route>
-          <Route element={<div>Map</div>}>
-            <Route
-              path="/agency/:agency/mode/3/routes/:route/direction/:direction/stops/:stop/map"
-              element={<BusMap />}
-            />
+
+            <Route element={<Predictions />}>
+              <Route path="/agency/:agency/mode/:mode/routes" element={<div>Routes</div>} />
+              <Route path="/agency/:agency/mode/:mode/routes/:route" element={<div>Stop</div>} />
+              <Route
+                path="/agency/:agency/mode/:mode/routes/:route/stops/:stop"
+                element={<div>Direction</div>}
+              />
+              <Route
+                path="/agency/:agency/mode/:mode/routes/:route/stops/:stop/direction/:direction"
+                element={<div>Predictions</div>}
+              />
+            </Route>
+            <Route element={<div>Map</div>}>
+              <Route
+                path="/agency/:agency/mode/3/routes/:route/direction/:direction/stops/:stop/map"
+                element={<BusMap />}
+              />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
