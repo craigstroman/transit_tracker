89f82cfb482b60f0361f5e7ed90a3179ed02ab0f
Craig Stroman <cstroman@gmail.com>
2023-07-02 12:50:48 -0400

    Creating predictions component

--- client/containers/predictions/Predictions.tsx
+++ client/containers/predictions/Predictions.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Routes } from '../../components/routes/Routes';
 
-export const Predictions = () => {
-  return <div>Predictions</div>;
+export const Predictions: React.FC = () => {
+  return (
+    <div className="row_small">
+      <div>
+        <Routes />
+      </div>
+    </div>
+  );
 };
