c37b1271b2ab350a1af9a10498d261092aff8554
Craig Stroman <cstroman@gmail.com>
2023-07-02 12:52:08 -0400

    Updating routes

--- server/routes/api/wmata/bus/index.js
+++ server/routes/api/wmata/bus/index.js
@@ -1,21 +1,28 @@
 import { Router } from 'express';
 import {
-  getRoutes, getDirections, getStops, getPredictions, getRouteCoordinates, getBusPositions
+  getRoutes,
+  getDirections,
+  getStops,
+  getPredictions,
+  getRouteCoordinates,
+  getBusPositions,
 } from '../../../../controllers/wmata/bus/index';
 
 const busRouter = new Router();
 const agency = '/wmata';
 
-busRouter.route(`${agency}/mode/3/routes`).get(getRoutes);
+busRouter.route(`${agency}/mode/bus/routes`).get(getRoutes);
 
-busRouter.route(`${agency}/mode/3/routes/:route/direction`).get(getDirections);
+busRouter.route(`${agency}/mode/bus/routes/:route/direction`).get(getDirections);
 
-busRouter.route(`${agency}/mode/3/routes/:route/direction/:direction/stops`).get(getStops);
+busRouter.route(`${agency}/mode/bus/routes/:route/direction/:direction/stops`).get(getStops);
 
-busRouter.route(`${agency}/mode/3/routes/:route/direction/:direction/stops/:stop/predictions`).get(getPredictions);
+busRouter
+  .route(`${agency}/mode/bus/routes/:route/direction/:direction/stops/:stop/predictions`)
+  .get(getPredictions);
 
-busRouter.route(`${agency}/mode/3/routes/:route/direction/:direction/coords`).get(getRouteCoordinates);
+busRouter.route(`${agency}/mode/bus/routes/:route/direction/:direction/coords`).get(getRouteCoordinates);
 
-busRouter.route(`${agency}/mode/3/routes/:route/direction/:direction/positions`).get(getBusPositions);
+busRouter.route(`${agency}/mode/bus/routes/:route/direction/:direction/positions`).get(getBusPositions);
 
 export default busRouter;
