e1f92d40de64778601690a30a0ab709e2084928a
Craig Stroman <cstroman@gmail.com>
2023-07-16 15:37:21 -0400

    Removing directions and predictions imports

--- client/containers/options/Options.tsx
+++ client/containers/options/Options.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Agency } from '../../components/agency/Agency';
 import { Mode } from '../../components/mode/Mode';
 import { Routes } from '../../components/routes/Routes';
-import { Directions } from '../../components/direction/Directions';
 import { Stops } from '../../components/stops/Stops';
-import { Predictions } from '../predictions/Predictions';
 
 export const Options: React.FC = () => {
   return (
