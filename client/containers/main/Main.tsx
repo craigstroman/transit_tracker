b42246f342f831a3d87612df87dae0175a36ae57
Craig Stroman <cstroman@gmail.com>
2023-07-30 22:15:55 -0400

    Adding showMap boolean

--- client/containers/main/Main.tsx
+++ client/containers/main/Main.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Options } from '../options/Options';
 import { PredictionsContainer } from '../predictions/Predictions';
+import { MapContainer } from '../map/Map';
 import './Main.scss';
 
 export const Main: React.FC = () => {
+  const location = useLocation();
+  const showMap = location.pathname.indexOf('/map') > -1 ? true : false;
+
   return (
     <div className="content">
       <header>
@@ -19,6 +24,7 @@ export const Main: React.FC = () => {
         <div className="predictions-row">
           <PredictionsContainer />
         </div>
+        <div className="map-row">{showMap && <MapContainer />}</div>
       </main>
     </div>
   );
