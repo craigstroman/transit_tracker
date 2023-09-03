388a516df2f5ffea70d85a23377efc2916dfcb4a
Craig Stroman <cstroman@gmail.com>
2023-09-03 16:58:50 -0400

    Updating routes

--- client/containers/App.tsx
+++ client/containers/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { BrowserRouter, Route, Routes as ReactRoutes } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 import { Main } from './main/Main';
 import { Agency } from '../components/agency/Agency';
-import { Routes } from '../components/routes/Routes';
+import { BusRoutes } from '../components/bus/routes/Routes';
 import { Mode } from '../components/mode/Mode';
-import { Stops } from '../components/stops/Stops';
-import { Directions } from '../components/direction/Directions';
+import { BusStops } from '../components/bus/stops/Stops';
+import { BusDirections } from '../components/bus/direction/Directions';
 import { PredictionsContainer } from './predictions/Predictions';
 import { MapContainer } from './map/Map';
 import './App.scss';
@@ -17,16 +17,19 @@ const element = document.getElementById('app');
 const root = createRoot(element as HTMLDivElement);
 
 root.render(
-  <>
+  <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <ReactRoutes>
           <Route path="/" element={<Main />}>
             <Route path="/" element={<Mode />} />
             <Route path="/mode/:mode/agency" element={<Agency />} />
-            <Route path="/mode/:mode/agency/:agency/routes" element={<Routes />} />
-            <Route path="/mode/:mode/agency/:agency/routes/:route" element={<Directions />} />
-            <Route path="/mode/:mode/agency/:agency/routes/:route/direction/:direction" element={<Stops />} />
+            <Route path="/mode/:mode/agency/:agency/routes" element={<BusRoutes />} />
+            <Route path="/mode/:mode/agency/:agency/routes/:route" element={<BusDirections />} />
+            <Route
+              path="/mode/:mode/agency/:agency/routes/:route/direction/:direction"
+              element={<BusStops />}
+            />
             <Route
               path="/mode/:mode/agency/:agency/routes/:route/direction/:direction/stops/:stop/predictions/:predictions"
               element={<PredictionsContainer />}
@@ -39,5 +42,5 @@ root.render(
         </ReactRoutes>
       </BrowserRouter>
     </Provider>
-  </>,
+  </StrictMode>,
 );
