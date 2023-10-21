5cfae402016e70332b48d4b961d56d04d0de75fb
Craig Stroman <cstroman@gmail.com>
2023-10-21 14:04:59 -0400

    Code cleanup

--- server/utils/general/index.js
+++ server/utils/general/index.js
@@ -6,14 +6,11 @@
  * @return     {Array}   The array with duplicate values removed.
  */
 export function removeDuplicates(dupArr, key) {
-  console.log('removeDuplicates: ');
   let result = [];
 
   result = dupArr.filter((obj, pos, arr) => {
-    return arr.map(mapObj => mapObj[key]).indexOf(obj[key]) === pos;
+    return arr.map((mapObj) => mapObj[key]).indexOf(obj[key]) === pos;
   });
-
-  console.log('result: ', result);
 
   return result;
 }
